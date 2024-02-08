8e965705fe70fabaee259f914b10cbd4c967945a
Rodinberg <rodinbergappiah08@gmail.com>
2024-02-07 21:41:45 -0800

    correction

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,26 +1,14 @@
 #!/usr/bin/python3
-
-"""
-<<<<<<< HEAD
-Python Module created by @Phina
-=======
-Python Module created by phinafati
->>>>>>> b0203d7989c3251a49a6f434a773a08a5675cf8b
-"""
+"""The write_file function container"""
 
 
 def write_file(filename="", text=""):
+    """write_file writes a string to a text file.
+        Args:
+            filename (str): name of file.
+            text (str): text to be written.
+            Return: the number of characters written
     """
-    function that writes text to a utf-8 encoded file
-    Arguments:
-        filename (str): The name of the file
-        text (str): The text to write
-    Return:
-        A file with text written
-    """
-    with open(filename, 'w', encoding='utf-8') as my_file:
-<<<<<<< HEAD
-        return my_file.write(text)
-=======
-        return my_file.write(text)
->>>>>>> b0203d7989c3251a49a6f434a773a08a5675cf8b
+    with open(filename, "w", encoding="utf-8") as file:
+        files = file.write(text)
+        return (files)
